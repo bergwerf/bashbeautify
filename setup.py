76f08ae12175203c43e3cc937303ffac87288744
Herman Bergwerf <hermanbergwerf@gmail.com>
2015-11-12 23:27:22 +0100

    Add initial script and setup

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name = 'bashbeautify',
+    version = '0.0.1',
+    description = 'A beautifier for Bash shell scripts written in Python.',
+    license = 'GPLv2',
+    author = 'Paul Lutus',
+    url = 'https://github.com/hermanbergwerf/bashbeautify',
+    classifiers = [
+        'Environment :: Console',
+        'Intended Audience :: Developers'
+    ],
+    keywords = ['automation', 'bash', 'format', 'beautify'],
+    py_modules = ['bashbeautify'],
+    entry_points = {'console_scripts': ['bashbeautify = bashbeautify:main']},
+)
