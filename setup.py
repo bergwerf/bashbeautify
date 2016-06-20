c37b52e71c22db91ba9132b6bcb4cdeeec44d71a
Herman Bergwerf <hermanbergwerf@gmail.com>
2016-06-20 13:51:24 +0200

    Bump version to 0.1.1
    PyPI does not support the previous version scheme.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = 'bashbeautify',
-    version = '0.1.0+1',
+    version = '0.1.1',
     description = 'A beautifier for Bash shell scripts written in Python.',
     license = 'GPLv2',
     author = 'Herman Bergwerf',
