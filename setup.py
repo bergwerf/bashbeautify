cb26d0c3c752cc4fec179717b42f946ce9192598
Herman Bergwerf <hermanbergwerf@gmail.com>
2016-08-24 23:42:15 +0200

    Bump version to 0.1.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = 'bashbeautify',
-    version = '0.1.3',
+    version = '0.1.4',
     description = 'A beautifier for Bash shell scripts written in Python.',
     license = 'GPLv2',
     author = 'Herman Bergwerf',
